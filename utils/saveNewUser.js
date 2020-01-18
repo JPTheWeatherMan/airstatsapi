b74f2b074c5f3a0d3a77a6dc7747caca1ab9d2c0
Jarrett <jarrettphale@gmail.com>
2020-01-18 14:21:36 -0500

    returns correnct data

--- utils/saveNewUser.js
+++ utils/saveNewUser.js
@@ -3,8 +3,8 @@ const User = require('../schemas/User')
 
 function saveNewUser(email){
   let uuid = uuidv4()
-  console.log('i am running')
   User.create({email: email, apikey: uuid})
+  return {email: email, apikey:uuid}
 }
 
 module.exports = saveNewUser

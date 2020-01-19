bb988c8eed445a78a92731ebdc011e2554e3ee4c
Jarrett <jarrettphale@gmail.com>
2020-01-19 14:18:56 -0500

    Fixed email not saving to db

--- routes/newUserRoute.js
+++ routes/newUserRoute.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const saveNewUser = require('../utils/saveNewUser')
+const uuidv4 = require('uuid/v4')
+const User = require('../schemas/User')
 const bodyParser = require('body-parser')
 
 const newUserRoute = express.Router();
@@ -7,9 +9,34 @@ newUserRoute.use(bodyParser.urlencoded({
   extended: true
 }));
 
-newUserRoute.post('/', function (req, res) {
-  const savedUser = saveNewUser(req.body.email)
-  res.status(201).send(savedUser)
+newUserRoute.post('/',  function (req, res) {
+  let email = req.body.email
+  let uuid = uuidv4()
+  const user = new User({
+    email: email,
+    apikey: uuid
+  })
+
+  user.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: "Handling POST requests to /user",
+      createdProduct: result
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
+
+
+  // User.create({email:email, apikey:uuid}, (user, err) => {
+  //   if(user){
+  //   res.status(201).json(user)
+  //   }
+  // })
 })
 
 module.exports = newUserRoute
